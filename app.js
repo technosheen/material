a79c7018c11867cb1cd496e0460b39ad0a288288
Glitch (material-starter-kit) <none>
2019-02-27 23:56:02 +0000

    🐷🐉 Checkpoint ./index.html:2420423/22 ./my-variables.js:2420423/40 ./app.js:2420423/162

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
 import {MDCRipple} from '@material/ripple/index';
 
+// import {teamName} from 'my-variables';
+
+// document.querySelector('.team-name').textContent = teamName;
+
 const ripple = new MDCRipple(document.querySelector('.mdc-button'));
---
+++ my-variables.js
@@ -0,0 +1,1 @@
+const teamName = 'Team name...';
