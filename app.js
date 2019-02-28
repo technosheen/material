94d520ae5dc133346f86b9f0de85d3da109b2e44
Glitch (material-starter-kit) <none>
2019-02-28 00:28:05 +0000

    🛋🖐 Checkpoint ./index.html:2420423/476 ./app.scss:2420423/39 ./app.js:2420423/304

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+import {MDCChipSet} from '@material/chips';
 import {MDCRipple} from '@material/ripple/index';
 import {MDCTextField} from '@material/textfield';
 
@@ -8,8 +9,10 @@ document.querySelector('.team-name').textContent = teamName;
 
 // Select HTML components
 const buttonEls = Array.from(document.querySelectorAll('.mdc-button'));
+const chipSetEl = document.querySelector('.mdc-chip-set');
 const textFieldEl = document.querySelector('.mdc-text-field');
 
 // Instantiate all components
 buttonEls.forEach((el) => new MDCRipple(el));
+new MDCChipSet(chipSetEl);
 new MDCTextField(textFieldEl);
