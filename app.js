25c8ae048703bce52665fedb464a8b656f8156ec
Glitch (material-starter-kit) <none>
2019-03-07 21:58:25 +0000

    🎹👌 Checkpoint ./index.html:2420423/2922 ./app.scss:2420423/364 ./app.js:2420423/1434 ./package.json:2420423/64

--- app.js
+++ app.js
@@ -1,15 +1,24 @@
+import {MDCCheckbox} from '@material/checkbox';
 import {MDCRipple} from '@material/ripple/index';
+import {MDCSwitch} from '@material/switch';
 import {MDCTextField} from '@material/textfield';
+import {MDCTextFieldHelperText} from '@material/textfield/helper-text';
 
 // Set text in HTML
 // document.querySelector('.glitch-url').textContent = process.env.PROJECT_ID;
 
 // Select HTML components
 const buttonEls = Array.from(document.querySelectorAll('.mdc-button'));
-const fabEls = Array.from(document.querySelectorAll('.mdc-fab'));
 const textFieldEl = document.querySelector('.mdc-text-field');
+// const helperTextEl = document.querySelector('.mdc-text-field-helper-text');
+const fabEls = Array.from(document.querySelectorAll('.mdc-fab'));
+const checkboxEls = Array.from(document.querySelectorAll('.mdc-checkbox'));
+const switchEls = Array.from(document.querySelectorAll('.mdc-switch'));
 
 // Instantiate all components
 buttonEls.forEach((el) => new MDCRipple(el));
-fabEls.forEach((el) => new MDCRipple(el));
 new MDCTextField(textFieldEl);
+// new MDCTextFieldHelperText(helperTextEl);
+fabEls.forEach((el) => new MDCRipple(el));
+checkboxEls.forEach((el) => new MDCCheckbox(el));
+switchEls.forEach((el) => new MDCSwitch(el));
