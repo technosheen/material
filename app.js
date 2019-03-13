9416e549d620237e3d1675b277acbc5f2ed15ff3
Glitch (material-starter-kit) <none>
2019-03-13 18:46:45 +0000

    🌨✨ Checkpoint ./index.html:2420423/1276 ./app.js:2420423/575

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 import {MDCCheckbox} from '@material/checkbox';
+import {MDCFormField} from '@material/form-field';
 import {MDCRipple} from '@material/ripple/index';
 import {MDCSwitch} from '@material/switch';
 import {MDCTextField} from '@material/textfield';
@@ -8,7 +9,13 @@ import {MDCTopAppBar} from '@material/top-app-bar';
 // Set text in HTML
 // document.querySelector('.glitch-url').textContent = process.env.PROJECT_ID;
 
+// Inside the drawer
+const formField = new MDCFormField(document.querySelector('.mdc-form-field'));
+const rtlSwitch = new MDCSwitch(document.querySelector('.rtl-switch'));
+formField.input = rtlSwitch;
+
 // Select HTML components
+const formFieldEl = document.querySelector('.mdc-form-field');
 const buttonEls = Array.from(document.querySelectorAll('.mdc-button'));
 const textFieldEl = document.querySelector('.mdc-text-field');
 // const helperTextEl = document.querySelector('.mdc-text-field-helper-text');
@@ -25,3 +32,4 @@ fabEls.forEach((el) => new MDCRipple(el));
 checkboxEls.forEach((el) => new MDCCheckbox(el));
 switchEls.forEach((el) => new MDCSwitch(el));
 new MDCTopAppBar(topAppBarEl);
+new MDCFormField(formFieldEl);
