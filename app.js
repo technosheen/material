3cbb44b7776f93d30c7c74a73c8e23708e49c273
Glitch (material-theme-builder) <none>
2019-04-12 05:41:03 +0000

    🎌🏔 Checkpoint ./app.scss:319521/777 ./app.js:319521/745

--- app.js
+++ app.js
@@ -209,3 +209,24 @@ const layoutForScreenSize = () => {
 
 window.addEventListener('resize', layoutForScreenSize);
 layoutForScreenSize();
+
+// Color Visualization
+const rgbToHex = (col) => {
+  if(col.charAt(0)=='r') {
+    col=col.replace('rgb(','').replace(')','').split(',');
+    var r=parseInt(col[0], 10).toString(16);
+    var g=parseInt(col[1], 10).toString(16);
+    var b=parseInt(col[2], 10).toString(16);
+    r=r.length==1?'0'+r:r; g=g.length==1?'0'+g:g; b=b.length==1?'0'+b:b;
+    var colHex='#'+r+g+b;
+    return colHex;
+  }
+}
+
+[...document.querySelectorAll('.theme-summary-color')].forEach((elem) => {
+  // Get background value of color component and sanitize
+  const bgColor = rgbToHex(getComputedStyle(elem).backgroundColor) || ''
+
+  // Append text of the element adjacent sibling to the end of the text string
+  elem.previousElementSibling.innerHTML += `: ${bgColor};`
+})
