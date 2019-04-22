ceabdb0a4457388d79a64c6b2d882d8716755291
Glitch (material-theme-builder) <none>
2019-04-22 17:12:46 +0000

    ⛸📚 Checkpoint ./index.html:319521/31482 ./app.js:319521/2716 ./app.scss:319521/384

--- app.js
+++ app.js
@@ -210,6 +210,19 @@ const layoutForScreenSize = () => {
 window.addEventListener('resize', layoutForScreenSize);
 layoutForScreenSize();
 
+// Build out shape size visualizer
+[...document.querySelectorAll('.theme-summary-shape')].forEach((elem) => {
+  // Get background value of color component and sanitize
+  const sizeVals = getComputedStyle(elem).borderRadius
+  const node = document.createElement("span");
+  const textnode = document.createTextNode(`${sizeVals};`);
+  node.classList.add('varValue');
+  node.appendChild(textnode); 
+
+  // Append text of the element adjacent sibling to the end of the text string
+  elem.previousElementSibling.appendChild(node)
+})
+
 // Color Visualization
 const rgbToHex = (col) => {
   if(col.charAt(0)=='r') {
@@ -223,10 +236,28 @@ const rgbToHex = (col) => {
   }
 }
 
+// Build out color variable visualizer
 [...document.querySelectorAll('.theme-summary-color')].forEach((elem) => {
   // Get background value of color component and sanitize
   const bgColor = rgbToHex(getComputedStyle(elem).backgroundColor) || ''
+  const node = document.createElement("span");
+  const textnode = document.createTextNode(`: ${bgColor};`);
+  node.classList.add('varValue');
+  node.appendChild(textnode); 
 
   // Append text of the element adjacent sibling to the end of the text string
-  elem.previousElementSibling.innerHTML += `: ${bgColor};`
+  elem.previousElementSibling.appendChild(node)
 })
+
+// Build out typography visualizer
+const setVisibleFont = () => {
+  const fontSample = document.querySelector('.mdc-typography--headline1')
+  const fontUrl = `https://fonts.google.com/specimen/${getComputedStyle(fontSample).fontFamily.split(',', 1)[0].replace(/ /g, '+').replace(/['"]+/g, '')}`
+  
+  // Set actual CSS
+  document.querySelector('.font-name').innerHTML = getComputedStyle(fontSample).fontFamily
+  
+  // Set Link
+  document.querySelector('.font-link').href = fontUrl
+}
+setVisibleFont()
